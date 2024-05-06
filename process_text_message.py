cd26349fa9aef18e3a444ea1a704e356157f7dc5
Fizikaman <fizikaman26@gmail.com>
2024-05-06 16:00:46 +0300

    Голосовые сообщения

---
+++ process_text_message.py
@@ -0,0 +1,47 @@
+import base64
+
+from openai import AsyncOpenAI
+
+import config
+
+memory = config.MEMORY_DICT  # словарь для хранения ответов
+client = AsyncOpenAI(api_key=config.CHAT_GPT_TOKEN)
+
+
+async def process(prompt, user_id, image_content=None) -> str:
+    model = "gpt-3.5-turbo"
+    max_tokens = None
+
+    if user_id not in memory:
+        memory[user_id] = []
+        memory[user_id].append({"role": "user", "content": prompt})
+    else:
+        memory[user_id].append({"role": "user", "content": prompt})
+
+    if image_content is not None:
+        model = "gpt-4-vision-preview"
+        max_tokens = 4000
+        base64_image_content = base64.b64encode(image_content).decode("utf-8")
+        base64_image_content = f"data:image/jpeg;base64,{base64_image_content}"
+        memory[user_id].append(
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt},
+                    {"type": "image_url", "image_url": {"url": base64_image_content}},
+                ],
+            }
+        )
+
+    try:
+        chat_completion = await client.chat.completions.create(  # Ожидаем завершения корутины
+            model=model, messages=memory[user_id], max_tokens=max_tokens
+        )
+        ai_response = chat_completion.choices[0].message.content
+        memory[user_id].append({"role": "assistant", "content": ai_response})
+        return ai_response
+    except Exception as e:
+        if type(e).__name__ == "BadRequestError":
+            del memory[user_id]
+        else:
+            raise e
--- routers.py
+++ routers.py
@@ -1,17 +1,19 @@
-import asyncio
 import logging
+import os
 from datetime import datetime
 
 from aiogram.enums import ChatAction
 from aiogram.filters import Command
-from aiogram.utils.chat_action import ChatActionSender
 from aiogram import Bot, Router, types, F
+from openai import AsyncOpenAI
 
 from chat_with_gpt import ans_from_gpt
 import config
+from process_text_message import process
 
 router = Router(name=__name__)
 logger = logging.getLogger(__name__)
+client = AsyncOpenAI(api_key=config.CHAT_GPT_TOKEN)
 
 
 @router.message(F.text, Command("start"))
@@ -54,3 +56,37 @@ async def conversation_with_gpt(message: types.Message, bot: Bot):
     response = await ans_from_gpt(message.from_user.id, message.text)
     await message.reply(response)
     await bot.delete_message(message.chat.id, msg.message_id)
+
+
+@router.message(F.voice)
+async def voice(message, bot: Bot):
+    file_info = await bot.get_file(message.voice.file_id)
+    downloaded_file = await bot.download_file(file_info.file_path)
+    os.makedirs("/tmp", exist_ok=True)
+    await message.bot.send_chat_action(
+        chat_id=message.chat.id, action=ChatAction.RECORD_VOICE
+    )
+    try:
+        response = await client.audio.transcriptions.create(
+            file=("file.ogg", downloaded_file, "audio/ogg"),
+            model="whisper-1",
+        )
+        ai_response = await process(response.text, message.from_user.id)
+        ai_voice_response = await client.audio.speech.create(
+            input=ai_response,
+            voice="nova",
+            model="tts-1-hd",
+            response_format="opus",
+        )
+        with open("/tmp/ai_voice_response.ogg", "wb") as f:
+            f.write(ai_voice_response.content)
+    except Exception as e:
+        await message.reply(f"Произошла ошибка, попробуйте позже!")
+        return e
+
+    await bot.send_voice(
+        message.chat.id,
+        voice=types.FSInputFile(os.path.join(config.BASE_DIR.parent, "/tmp/ai_voice_response.ogg")),
+        reply_to_message_id=message.message_id,
+    )
+
